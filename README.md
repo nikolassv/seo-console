159748b353443e5a0b9840862cc4b70d7d90c1da
Nikolas Schmidt-Voigt <n.schmidtvoigt@googlemail.com>
2013-04-11 18:05:08 +0200

    added README and enabled the use of pipes with serpnum.sh and serps.sh

---
+++ README.md
@@ -0,0 +1,9 @@
+bash scripts for recurring SEO tasks
+====================================
+
+just a small collection of bash scripts for SEO tasks like
+
+- scraping google search results (serps.sh)
+- getting the number of results for a keyword (serpnum.sh)
+- checking status codes and redirects (httpstatus.sh)
+- extracting html meta tags from pages (htmlmeta.sh)
--- serps.sh
+++ serps.sh
@@ -6,15 +6,11 @@
 
 SCRIPTNAME=serpnum.sh
 
-usage () {
-  echo "Usage: $SCRIPTNAME keyword"
-}
 
-if (( $# == 1 ))
-then
-  curl "http://www.google.de/search" --data-urlencode q="$1" -G -A 'Mozilla/5.0' -s | grep -o '<h3 class="r"><a href="/url[^''"]*"' | sed 's/.*url.q=\([^"&]*\).*/\1/'
-else
-  usage
-fi
+while read keyword
+do
+  curl "http://www.google.de/search" --data-urlencode q="$keyword" -G -A 'Mozilla/5.0' -s | grep -o '<h3 class="r"><a href="/url[^''"]*"' | sed 's/.*url.q=\([^"&]*\).*/\1/'
+  sleep 3s
+done
 
 exit 0
